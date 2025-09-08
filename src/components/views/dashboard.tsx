fb528f0645ba4c39e48c8d16ccffe714dec632e1
Afsheen Ara <afsheenara1024@gmail.com>
2025-09-08 19:51:30 +0530

    Initial commit

---
+++ src/components/views/dashboard.tsx
@@ -0,0 +1,61 @@
+import React, { useMemo } from 'react';
+import { WeatherData } from '@/types/weather';
+import DayDuration from '@/components/views/day-duration';
+import AirPollutionChart from '@/components/views/AirPollution';
+import TemperatureHumidityChart from '@/components/views/temp-humidity';
+import ClientMap from '@/components/views/client-map';
+import CurrentWeatherCard from '@/components/views/current-weather';
+import WindPressureCard from '@/components/views/wind-pressure';
+import HourlyForecast from '@/components/views/hourly-forecast';
+
+interface WeatherDashboardProps {
+  weatherData: WeatherData;
+  unit: 'metric' | 'imperial';
+}
+
+// Dashboard is a React component that aggregates and displays weather data in various cards and charts.
+// It receives weather data and unit as props and passes them to child components.
+const WeatherDashboard: React.FC<WeatherDashboardProps> = ({ weatherData, unit }) => {
+  const { currentWeather, forecast, airPollution } = weatherData;
+
+  // Memoize hourly forecast data for the first 5 items
+  const hourlyForecastData = useMemo(
+    () =>
+      forecast.list.slice(0, 5).map((item) => ({
+        time: new Date(item.dt * 1000).toLocaleTimeString([], {
+          hour: '2-digit',
+          minute: '2-digit',
+        }),
+        temperature: Math.round(item.main.temp),
+        weather: item.weather[0].main,
+      })),
+    [forecast.list],
+  );
+
+  return (
+    <div className="bg-inherit min-h-screen flex flex-col">
+      <div className="container grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        <CurrentWeatherCard
+          currentWeather={currentWeather}
+          forecast={forecast}
+          unit={unit}
+        />
+        <div className="grid grid-rows-2 gap-4">
+          <WindPressureCard currentWeather={currentWeather} unit={unit} />
+          <HourlyForecast forecast={hourlyForecastData} unit={unit} />
+        </div>
+        <AirPollutionChart data={airPollution} />
+        <TemperatureHumidityChart data={forecast} unit={unit} />
+        <DayDuration data={currentWeather} />
+        <ClientMap
+          center={[currentWeather.coord.lat, currentWeather.coord.lon]}
+          zoom={10}
+          markerPosition={[currentWeather.coord.lat, currentWeather.coord.lon]}
+          popupContent={`${currentWeather.name}, ${currentWeather.sys.country}`}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default WeatherDashboard;
